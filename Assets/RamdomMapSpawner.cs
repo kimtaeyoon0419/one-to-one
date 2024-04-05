20934dcb2064735f4bb10fb55b0b2154337813e3
kimtaeyun0419 <sdh230104@sdh.hs.kr>
2024-04-05 20:20:28 +0900

    Update12
    랜덤 맵 생성

---
+++ Assets/RamdomMapSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RamdomMapSpawner : MonoBehaviour
+{
+    [Header("½ºÆù À§Ä¡")]
+    public Transform[] spawnPos;
+
+    [Header("¸Ê ÇÁ¸®ÆÕ")]
+    public GameObject[] mapPerfab;
+
+    public List<int> randomIndex;
+
+    void Start()
+    {
+        RandomMapSpawn();
+    }
+
+    void RandomMapSpawn()
+    {
+        for (int i = 0; i < mapPerfab.Length; i++)
+        {
+            randomIndex.Add(i);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            int index = Random.Range(0, randomIndex.Count - 1);
+            GameObject instantiatedObject = Instantiate(mapPerfab[randomIndex[index]], spawnPos[i].position, Quaternion.identity);
+            instantiatedObject.transform.SetParent(this.transform);
+            randomIndex.RemoveAt(index);
+        }
+    }
+
+}
