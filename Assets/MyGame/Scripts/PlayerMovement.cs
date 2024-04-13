2b0feb051b0a502a5f06c357272272b88435fa4d
kimtaeyun0419 <sdh230104@sdh.hs.kr>
2024-04-14 02:06:15 +0900

    TestScene+
    스크립트 수정할 땐 테스트씬에서 먼저 해볼것

---
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Rigidbody")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("LayCast")]
+    [SerializeField] private Transform groundChk;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform wallChk;
+    [SerializeField] private LayerMask wallLayer;
+
+    [Header("WallJump")]
+    private bool iswallSliding;
+    private bool isWallJumping;
+    private float wallSlidingSpeed = 2f;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDiration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(18f, 16f);
+
+    private float hor; // hor = Input.GetAxis("Horizontal"); 용도
+    private bool isFacingRight = true; // Flip 용도
+
+    Vector2 velocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+
+        if (!isWallJumping)
+        {
+            Flip();
+        }
+
+        Jump();
+        wallSlide();
+        WallJump();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isWallJumping)
+        {
+            Move();
+        }
+    }
+    private void Move() // 플레이어 움직임
+    {
+        velocity.x = hor * PlayerStatManager.instance.speed;
+        velocity.y = rb.velocity.y;
+
+        rb.velocity = velocity; // new를 지양하기 위해 Vector2 velocity 선언 후 초기화
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, PlayerStatManager.instance.JumpPoawer);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundChk.position, 0.2f, groundLayer);
+        // 만약 groundChk.position에 위치한 0.2f 크기의 원 안에 groundLayer가 부딪힌다면 true를 리턴
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallChk.position, 0.2f, wallLayer);
+        // 만약 wallChk.position에 0.2f 크기의 원 안에 wallLayer가 부딪힌다면 true를 리턴
+    }
+
+    private void wallSlide() // 벽 슬라이딩
+    {
+        if (IsWalled() && !IsGrounded() && hor != 0)
+        {
+            iswallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+            // velocity의 y값을 wallSlidingSpeed만큼 내려가기 위해 앞에 - 를 붙힘
+        }
+        else
+        {
+            iswallSliding = false;
+        }
+    }
+
+    private void WallJump()
+    {
+        if (iswallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDiration = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDiration * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDiration)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDiration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && hor < 0f || !isFacingRight && hor > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+}
