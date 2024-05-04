0bd91c555bfe78884c06181ad0af3bcb6c0ca12e
kimtaeyun0419 <sdh230104@sdh.hs.kr>
2024-05-05 01:57:09 +0900

    Monster Player Follow, Monster type++
    몬스터 Follow Ai 수정해야함

--- Assets/MyGame/01.Scripts/02.Monster/Monster.cs
+++ Assets/MyGame/01.Scripts/02.Monster/Monster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class Monster : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public abstract class Monster : MonoBehaviour
     protected Animator animator;
     protected SpriteRenderer sr;
     protected Transform target_Player;
+    protected NavMeshAgent agent;
     Color hafpA = new Color(1, 1, 1, 0.5f); // 피격 색전환 1번 ( 반투명 )
     Color fullA = new Color(1, 1, 1, 1); // 피격 색전환 2번 ( 원본색 )
 
@@ -23,11 +25,10 @@ public abstract class Monster : MonoBehaviour
     [Header("Move")]
     protected int nextMove; // 다음으로 움직일 방향
     private Vector2 frontVec; // 앞에 땅이 있는지 확인하는 거리
-    int rayLookDir; // 몬스터의 방향과 맞는 레이 방향
     protected bool isAttack = false; // 공격중인지
     private Vector3 velocity;
     protected float player_Distance;
-
+    protected bool isFollow = false;
 
     [Header("DropItem")]
     private DropItem itemdrop;
@@ -57,18 +58,23 @@ public abstract class Monster : MonoBehaviour
 
         rb.velocity = velocity; // new를 지양하기 위해 Vector2 velocity 선언 후 초기화
 
+        if (isFollow)
+        {
+            FollowPlayer();
+        }
         if (!isAttack)
         {
             frontVec = new Vector2(rb.position.x + nextMove, rb.position.y); // frontVec = 몬스터의 현재위치 + nextMove
-        }
-        Debug.DrawRay(frontVec, Vector2.down, new Color(0, 1, 0));
-        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector2.down, 3f, LayerMask.GetMask("Ground")); // frontVec만큼의 거리에 바닥이 있는지 검사
-        if (rayHit.collider == null) // forntVec만큼 떨어진 거리에 땅이 없다면 회전
-        {
-            Turn();
-            StartCoroutine(Co_StartThinkCoroutineDelay(1f));
+            Debug.DrawRay(frontVec, Vector2.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector2.down, 3f, LayerMask.GetMask("Ground")); // frontVec만큼의 거리에 바닥이 있는지 검사
+            if (rayHit.collider == null) // forntVec만큼 떨어진 거리에 땅이 없다면 회전
+            {
+                Turn();
+                StartCoroutine(Co_StartThinkCoroutineDelay(1f));
+            }
         }
     }
+
     protected virtual void Update()
     {
         player_Distance = Vector3.Distance(transform.position, target_Player.transform.position);
@@ -86,6 +92,20 @@ public abstract class Monster : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isFollow = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isFollow = false;
+        }
+    }
     #endregion
 
     #region Private_Function
@@ -94,28 +114,16 @@ public abstract class Monster : MonoBehaviour
         nextMove *= -1;
         StopCoroutine(Co_Think());
     }
-    /// <summary>
-    /// 플레이어가 있는지 검사하는 레이케이스
-    /// </summary>
-    protected void AttackRay() //레이케스트에 플레이어가 들어오면 공격!!!
+    private void FollowPlayer()
     {
-        if (nextMove != 0)
+        Debug.Log("따라가는중~~");
+        if (target_Player.transform.position.x - transform.position.x < 0.2f)
         {
-            rayLookDir = nextMove;
+            nextMove = -1;
         }
-        Debug.DrawRay(rb.position, Vector2.right * stat.atkRange * rayLookDir, Color.yellow); // 레이확인
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.right * rayLookDir, stat.atkRange, LayerMask.GetMask("Player"));
-
-        if (hit.collider != null && stat.curAtkSpeed <= 0)
+        else if (target_Player.transform.position.x - transform.position.x > 0.2f)
         {
-            stat.curAtkSpeed = stat.atkSpeed;
-            StopCoroutine(Co_Think()); // 코루틴을 꺼서 방향 전환을 막음
-            isAttack = true;
-            Attack(); // 공격 후
-            if (isAttack == false)
-            {
-                StartCoroutine(Co_StartThinkCoroutineDelay(2f)); // 다시 방향전환을 정함
-            }
+            nextMove = 1;
         }
     }
     #endregion
@@ -137,30 +145,25 @@ public abstract class Monster : MonoBehaviour
     }
     #endregion
 
-    #region Protected_Function
-    /// <summary>
-    /// 공격
-    /// </summary>
-    protected abstract void Attack();// 몬스터 공격
-    #endregion
 
     #region Coroutine_Function
-    IEnumerator Co_Think() // 어느방향으로 움직일지 생각해주는 코루틴(재귀)
+    protected IEnumerator Co_Think() // 어느방향으로 움직일지 생각해주는 코루틴(재귀)
     {
-        nextMove = Random.Range(-1, 2);
-
+        if (!isFollow)
+        {
+            nextMove = Random.Range(-1, 2);
+        }
         float nextThinkTime = Random.Range(2f, 5f);
         if (this.gameObject.activeSelf)
         {
-            yield return new WaitForSeconds(nextThinkTime);
-            yield return StartCoroutine(Co_Think());
+            yield return StartCoroutine(Co_StartThinkCoroutineDelay(nextThinkTime));
         }
         else
         {
             yield break;
         }
     }
-    IEnumerator Co_StartThinkCoroutineDelay(float delay) // Think() 코루틴을 일정 시간 뒤에 실행해줌
+    protected IEnumerator Co_StartThinkCoroutineDelay(float delay) // Think() 코루틴을 일정 시간 뒤에 실행해줌
     {
         yield return new WaitForSeconds(delay);
 
@@ -170,7 +173,7 @@ public abstract class Monster : MonoBehaviour
     /// 피격 했을 때 반짝거림
     /// </summary>
     /// <returns></returns>
-    IEnumerator Co_isHit() // 맞았을 때 색전환
+    protected IEnumerator Co_isHit() // 맞았을 때 색전환
     {
         for (int i = 0; i < 3; i++)
         {
--- Assets/MyGame/01.Scripts/02.Monster/MonsterS/Slime.cs
+++ Assets/MyGame/01.Scripts/02.Monster/MonsterS/Slime.cs
@@ -8,8 +8,9 @@ using UnityEngine;
 
 public class Slime : Monster
 {
-    public float JumpScale ;
+    public float JumpScale;
     private bool isGround = false;
+    float rayLookDir; // 몬스터의 방향과 맞는 레이 방향
 
     #region Unity_Function
     protected override void Awake()
@@ -40,8 +41,34 @@ public class Slime : Monster
     }
     #endregion
 
-    #region override_Function
-    protected override void Attack()
+    #region Private_Function
+    /// <summary>
+    /// 플레이어가 있는지 검사하는 레이케이스
+    /// </summary>
+    private void AttackRay() //레이케스트에 플레이어가 들어오면 공격!!!
+    {
+        if (nextMove != 0)
+        {
+            rayLookDir = nextMove * 0.6f;
+        }
+        Debug.DrawRay(rb.position, Vector2.right * stat.atkRange * rayLookDir, Color.yellow); // 레이확인
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.right * rayLookDir, stat.atkRange, LayerMask.GetMask("Player"));
+
+        if (hit.collider != null && stat.curAtkSpeed <= 0)
+        {
+            stat.curAtkSpeed = stat.atkSpeed;
+            StopCoroutine(Co_Think()); // 코루틴을 꺼서 방향 전환을 막음
+            isAttack = true;
+            Attack(); // 공격 후
+            if (isAttack == false)
+            {
+                StartCoroutine(Co_StartThinkCoroutineDelay(2f)); // 다시 방향전환을 정함
+            }
+        }
+    }
+
+
+    private void Attack()
     {
         if (isGround)
         {
